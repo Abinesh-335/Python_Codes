b5b6aaa182eba87def79f8dce4e08d5d2c56605e
Abinesh-335 <rkabinesh.3325@gmail.com>
2026-06-19 23:17:41 +0530

    feat:initial code

---
+++ Loops.py
@@ -0,0 +1,28 @@
+#Check password
+
+'''
+password="123@"
+check=""
+while True:
+    if check=="123@":
+        print("Access Granted")
+        break
+    check=input("Enter the password:")
+
+
+nums=[5,-2,6,1,-2,0]
+for i in nums:
+    if i<0:
+        continue
+    print(i)        # 5 6 1 0
+for i in nums:
+    pass            # Future logic implementation
+
+
+mul_no=int(input("Enter the Multiplier No:"))
+no_range=int(input("Enter the range:"))
+for i in range(10):
+    print(f"{i}x{no_range}={i*mul_no}")
+'''
+
+
