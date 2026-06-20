3aebf6e4fed55bf18d9f15a7c17de61c046c591d
Abinesh-335 <rkabinesh.3325@gmail.com>
2026-06-20 17:31:05 +0530

    feat:initial codes

--- Function.py
+++ Function.py
@@ -1,4 +1,13 @@
-'''
+def hello(name):
+    print("Welcome",name)
+hello("vinoth")              # Welcome vinoth
+
+
+def add(a,b):
+    return a+b
+result=add(5,5)
+print(result)               #10
+
 # *args
 
 def add(*sums):
@@ -6,15 +15,16 @@ def add(*sums):
     for num in sums:
         tot+=num
     return tot
-print(add(1,2,4,32,2))
+print(add(1,2,4,32,2))          #41
 
 
 # **Kargs
 
 def create_profile(**kargs):
+    print(kargs.items())                #dict_items([('name', 'Abinesh'), ('age', 23), ('gender', 'male')])
     for key,val in kargs.items():
-        print(f"{key}:{val}")
+        print(f"{key}:{val}")           #name:Abinesh age:23 gender:male
 
 create_profile(name="Abinesh",age=23,gender='male')
-'''
+
 
