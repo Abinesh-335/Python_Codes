3f128a82758a977d6d1eed354156b0f5191f56c9
Abinesh-335 <rkabinesh.3325@gmail.com>
2026-08-20 00:01:58 +0530

    feat:Leetcode

--- Testing.py
+++ Testing.py
@@ -32,6 +32,8 @@ print(strg[-3:-1]) #rl
 print(strg[2:-1]) #rl
 print(strg[2:4]) #rl
 '''
+from Tools.scripts.generate_re_casefix import alpha
+
 '''
 strg="This the id : uxwe123. please react"
 res=strg.split(':')[1].split('.')[0].strip()
@@ -152,8 +154,9 @@ s2="nagrram"
 print(obj.isAnagram(s1,s2))
 
 '''
-'''
+
 #METHOD 2
+'''
 class Solution:
     def isAnagram(self,stg,tar):
         if len(stg)!=len(tar):
@@ -174,3 +177,94 @@ s1="anagram"
 s2="nagaram"
 print(obj.isAnagram(s1,s2))
 '''
+'''
+#######// GROUP ANAGRAM //########
+class Solution:
+    def groupanagram(self,strs):
+        hash={}
+        for st in strs:
+            l="".join(sorted(st))
+            if l in hash:
+                hash[l].append(st)
+            else:
+                hash[l]=[st]
+
+        return list(hash.values())
+obj=Solution()
+s1=["eat","tea","tan","ate","nat","bat"]
+print(obj.groupanagram(s1))
+
+'''
+
+'''
+#######// Top K Frequent Element //########
+class Solution(object):
+    def topKFrequent(self, nums, k):
+
+        count = {}
+        for i in nums:
+            count[i] = count.get(i, 0) + 1
+        bucket = [[] for _ in range(len(nums)+1)]
+        res=[]
+        for v,f in count.items():
+            bucket[f].append(v)
+        for val in range(len(bucket)-1,0,-1):
+            for i in bucket[val]:
+                res.append(i)
+                if len(res)==k:
+                    return res
+obj=Solution()
+s1=[1,1,1,2,2,3]
+k = 2
+print(obj.topKFrequent(s1,k))
+
+'''
+#######// Valid Palindrome //########
+'''
+class Solution(object):
+    def isPalindrome(self, s):
+        s=s.lower()
+        new_s=""
+        for i in s:
+            if  i.isalnum():
+                new_s+=i
+        l=0
+        r=len(new_s)-1
+        print(new_s)
+        while l<r:
+            if new_s[l]!=new_s[r]:
+                return False
+            else:
+                l+=1
+                r-=1
+
+        return True
+
+obj = Solution()
+s1 = "0p"
+print(obj.isPalindrome(s1))
+
+'''
+
+#############// TWO POINTER //#################
+# Two pointers mostly used in sorted array
+#TWO SUM Example
+class Solution:
+    def twopointer(self,nums,t):
+        l=0
+        r=len(nums)-1
+        while l<r:
+            val=nums[l]+nums[r]
+            if val==t:
+                return [l,r]
+            elif val<t:
+                l+=1
+            elif val>t:
+                r-=1
+
+        return "No match"
+
+obj = Solution()
+s1 = [1,2,3,4,5,6]
+t=5
+print(obj.twopointer(s1,t))
